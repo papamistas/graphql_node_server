861465c0bab9af29445049cfd0199370746a6c7f
Leonel-Domingos <leonel.domingos@quad-systems.com>
2019-03-09 22:31:42 +0000

    social logins

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import resolvers from './resolvers';
 import db from './models';
 
 
+
+
 const server = new ApolloServer({
   typeDefs: gql(typeDefs),
   resolvers,
@@ -16,11 +18,11 @@ const server = new ApolloServer({
 const app = express ();
 server.applyMiddleware({ app });
 
-//app.use(express.static('app/public'));
+app.use(express.static('app/public'));
 
 db.sequelize.sync().then(() => {
   // populate author table with dummy data
-  db.author.bulkCreate(
+/*  db.author.bulkCreate(
     lodash.times(10, () => ({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
@@ -33,9 +35,78 @@ db.sequelize.sync().then(() => {
       content: faker.lorem.paragraph(),
       authorId: lodash.random(1, 10),
     })),
-  );
+  );*/
 
   app.listen({ port: 7000 }, () =>
     console.log(`🚀 Server ready at http://localhost:7000${server.graphqlPath}`),
   );
 });
+var passport = require('passport')
+    , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+        clientID: 405556700218895,
+        clientSecret: 'f5ff7f0d0de47222be4dcdcd82b65e2a',
+        callbackURL: "http://www.example.com/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        /*User.findOrCreate(..., function(err, user) {
+            if (err) { return done(err); }
+            done(null, user);
+        });*/
+    }
+));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/',
+        failureRedirect: '/login' }));
+
+
+
+
+var passport = require('passport');
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+        clientID: '150235012843-eibaqvkld98i32jl8heif2l4krervj5d.apps.googleusercontent.com',
+        clientSecret: '_aSDv_nJGwqzH2YFdlmkzFgs',
+        callbackURL: "http://localhost:7000/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+
+    }
+));
+
+
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
