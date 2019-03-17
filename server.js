a7e9c3955e3e6af9941abfef0d9f08ab444fdb63
Leonel-Domingos <leonel.domingos@quad-systems.com>
2019-03-17 17:50:23 +0000

    social logins, passport, sessions and logout working

--- server.js
+++ server.js
@@ -48,59 +48,7 @@ function checkAuth(req, res, next) {
     }
 }
 
-var Strategy = require('passport-facebook').Strategy;
-passport.use(new Strategy(config.fb,
-    function(accessToken, refreshToken, profile, cb) {
-        // In this example, the user's Facebook profile is supplied as the user
-        // record.  In a production-quality application, the Facebook profile should
-        // be associated with a user record in the application's database, which
-        // allows for account linking and authentication with other identity
-        // providers.
-        var User = db.sequelize.import('./models/cliente.js')
 
-        User.findOrCreate({where: {fb_id: profile.id}, defaults: {cliente: profile.displayName}})
-            .then(function (user) {
-                return cb(null, user);
-            })
-
-    }));
-
-
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-passport.use(new GoogleStrategy(config["ggl"],
-    function (accessToken, refreshToken, profile, cb) {
-
-        var User = db.sequelize.import('./models/cliente.js')
-        User.findOrCreate({where: {google_id: profile.id}, defaults: {cliente: profile.displayName}}).then( function (user, err) {
-            return cb(null, user);
-        });
-    }
-));
-
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-
-    function(username, password, done) {
-        var User = db.sequelize.import('./models/cliente.js')
-        User.findOne({where: {"email": username}}).then(function (user, err) {
-            {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    return done(null, false, {message: 'Incorrect username.'});
-                }
-                /*if (!user.validPassword(password)) {
-                    return done(null, false, {message: 'Incorrect password.'});
-                }*/
-                done(null,user);
-            }
-        });
-    }
-
-));
 
 
 passport.serializeUser(function(user, cb) {
